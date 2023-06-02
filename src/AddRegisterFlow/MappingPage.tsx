bbdc43005779b83181e899a5c17134193f2ed7bc
ashwinpc <ashwinrookie@gmail.com>
2023-06-02 09:38:45 +0530

    host

--- src/AddRegisterFlow/MappingPage.tsx
+++ src/AddRegisterFlow/MappingPage.tsx
@@ -3,6 +3,7 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 function MappingPage() {
   return (
     <>
@@ -13,7 +14,20 @@ function MappingPage() {
             id="uncontrolled-tab-example"
             className="mb-3"
           >
-            <Tab eventKey="home" title="Sheet 1"></Tab>
+            <Tab eventKey="home" title="Sheet 1">
+              <Row style={{ marginTop: "3rem" }}>
+                <Col style={{ padding: "0 6rem" }}>
+                  <label>Invoice Amount</label>
+                  <Form.Select aria-label="Default select example">
+                    <option>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </Col>
+                <Col></Col>
+              </Row>
+            </Tab>
             <Tab eventKey="profile" title="Sheet 2">
               Tab content for Profile
             </Tab>
